dc9d83892ec9042ffed7050abb12d8ef7da0230b
polygraph-umg <russell@polygraph.cool>
2023-06-21 12:08:32 -0700

    move options

--- popup/popup.js
+++ popup/popup.js
@@ -1,39 +1,39 @@
-(async () => {
-  const base = "pass-the-mic";
+// (async () => {
+//   const base = "pass-the-mic";
 
-  async function get(key) {
-    return new Promise((resolve, reject) => {
-      try {
-        chrome.storage.local.get(key, (value) => resolve(value[key]));
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
+//   async function getStorage(key) {
+//     return new Promise((resolve, reject) => {
+//       try {
+//         chrome.storage.local.get(key, (value) => resolve(value[key]));
+//       } catch (err) {
+//         reject(err);
+//       }
+//     });
+//   }
 
-  async function set(key, value) {
-    return new Promise((resolve, reject) => {
-      try {
-        chrome.storage.local.set({ [key]: value }, resolve);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
+//   async function setStorage(key, value) {
+//     return new Promise((resolve, reject) => {
+//       try {
+//         chrome.storage.local.set({ [key]: value }, resolve);
+//       } catch (err) {
+//         reject(err);
+//       }
+//     });
+//   }
 
-  async function init() {
-    const settings = [...document.querySelectorAll("input")].map(
-      (input) => input.id
-    );
-    settings.forEach(async (setting) => {
-      const key = `${base}-${setting}`;
-      const value = await get(key);
-      document.getElementById(setting).checked = value || false;
-      document.getElementById(setting).addEventListener("change", (e) => {
-        set(key, e.target.checked);
-      });
-    });
-  }
+//   async function init() {
+//     const settings = [...document.querySelectorAll("input")].map(
+//       (input) => input.id
+//     );
+//     settings.forEach(async (setting) => {
+//       const key = `${base}-${setting}`;
+//       const value = await getStorage(key);
+//       document.getElementById(setting).checked = value || false;
+//       document.getElementById(setting).addEventListener("change", (e) => {
+//         setStorage(key, e.target.checked);
+//       });
+//     });
+//   }
 
-  init();
-})();
+//   init();
+// })();
--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,34 @@
 const visPadding = 16;
-const minWidth = 128;
-const visEl = document.createElement("div");
+const minWidth = 160;
 
 const speakers = new Map();
 const speakerNodes = new Map();
 const debug = true;
+const base = "pass-the-mic";
 
-let numSpeakers = 0;
+let settings;
 let threshold = 0;
 let nameYou = "You";
+let captionsButtonEl;
+let captionsContainerEl;
+
+function setStorage(key, value) {
+  window.localStorage.setItem(`${base}-${key}`, value);
+}
+
+function getStorage(key) {
+  return window.localStorage.getItem(`${base}-${key}`);
+}
+
+// async function getStorage(key) {
+//   return new Promise((resolve, reject) => {
+//     try {
+//       chrome.storage.local.get(key, (value) => resolve(value[key]));
+//     } catch (err) {
+//       reject(err);
+//     }
+//   });
+// }
 
 function prepareData() {
   const all = [];
@@ -28,7 +48,9 @@ function prepareData() {
   percents.sort((a, b) => b.percent - a.percent);
 
   // TODO min width
-  const w = visEl.getBoundingClientRect().width - visPadding;
+  const w =
+    document.querySelector(".ptm-vis").getBoundingClientRect().width -
+    visPadding;
 
   const withWidth = percents.map((d) => ({
     ...d,
@@ -55,16 +77,32 @@ function prepareData() {
   return clean;
 }
 
+function updateNumSpeakers() {
+  const total = document.querySelectorAll("[data-self-name]").length;
+  const fac = d3.selectAll(".speaker.facilitator").size();
+  const numSpeakers = total - fac;
+  threshold = (1 / numSpeakers) * 1.5;
+}
+
+function toggleFacilitator() {
+  const value = d3.select(this).classed("facilitator");
+  d3.select(this).classed("facilitator", !value);
+  updateNumSpeakers();
+}
+
+function highlight(d) {
+  if (d3.select(this).classed("facilitator")) return false;
+  if (d.key === "Others") return false;
+  return d.percent >= threshold;
+}
+
 function updateVis() {
   const data = prepareData();
-
-  // const ranked = percents.map((d, i) => ({ ...d, rank: i }));
-  // if (debug) console.table(ranked);
 
   const speakerEnter = (enter) => {
     const speaker = enter.append("div");
 
-    speaker.attr("class", "speaker");
+    speaker.attr("class", "speaker").attr("aria-role", "button");
 
     speaker.append("div").attr("class", "bar");
 
@@ -80,17 +118,18 @@ function updateVis() {
     const percent = label.append("span").attr("class", "percent").text("0%");
     percent.text((d) => d3.format(".0%")(d.percent));
 
+    speaker.on("click", toggleFacilitator);
     return speaker;
   };
 
   const joined = d3
-    .select(visEl)
+    .select(".ptm-vis")
     .selectAll(".speaker")
     .data(data, (d) => d.key)
     .join(speakerEnter);
 
   joined
-    .classed("highlight", (d) => d.percent >= threshold)
+    .classed("highlight", highlight)
     .style("width", (d) => d3.format(".1%")(d.percent));
 
   joined.select(".percent").text((d) => d3.format(".0%")(d.percent));
@@ -204,9 +243,7 @@ function updateSpeakers() {
   //   if (!exists) speakers.set(id, []);
   //   console.log(id);
   // });
-  numSpeakers = document.querySelectorAll("[data-self-name]").length;
-  threshold = (1 / numSpeakers) * 1.5;
-  // console.log(numSpeakers);
+  updateNumSpeakers();
 }
 
 function handlePersonChange(mutationsList) {
@@ -230,19 +267,172 @@ function getYouName() {
   nameYou = a ? a.substring(a.lastIndexOf('"') + 1, a.length) : "You";
 }
 
-function init(captionsButton) {
+function updateOptions() {
+  const opts = {};
+  settings.forEach((key) => {
+    const value = getStorage(key);
+    opts[key] = value;
+    console.log({ key, value });
+  });
+
+  console.log(opts);
+
+  const visEl = document.querySelector(".ptm-vis");
+
+  const { display } = window.getComputedStyle(captionsContainerEl);
+
+  d3.select(captionsContainerEl).style(
+    "opacity",
+    opts.captions === "true" ? 1 : 0
+  );
+
+  if (opts.enable === "true") {
+    if (display == "none" && captionsButtonEl) captionsButtonEl.click();
+    if (!visEl) observeCaptions();
+  } else {
+    // if we were previously running it AND captions are visible, hide them
+    if (visEl && display !== "none") {
+      if (captionsButtonEl) captionsButtonEl.click();
+      d3.select(".ptm-vis").remove();
+    }
+    // TODO disconnect observer
+  }
+}
+
+// function storageChanged(changes) {
+//   for (let [key, { newValue }] of Object.entries(changes)) {
+//     options[key.replace(base, "")] = newValue;
+//   }
+//   updateOptions();
+// }
+
+function observeCaptions() {
+  const visEl = document.createElement("div");
   visEl.classList.add("ptm-vis");
   document.body.appendChild(visEl);
-
-  captionsButton.click();
 
   getYouName();
   // TODO hide captions (if option enabled)
 
-  const el = document.querySelector(".a4cQT").childNodes[0].childNodes[0];
+  const el = captionsContainerEl.childNodes[0].childNodes[0];
   const config = { attributes: false, childList: true, subtree: false };
   const observer = new MutationObserver(handlePersonChange);
   observer.observe(el, config);
+}
+
+function toggleOptions() {
+  const el = ".ptm-popup .options";
+  d3.select(el).classed("active", !d3.select(el).classed("active"));
+}
+
+function createPopup() {
+  const margin = 8;
+  const outer = 96;
+  const w = outer - margin;
+  const w1 = w - 24;
+  const w2 = w - 16;
+
+  const popup = d3.select("body").append("div").attr("class", "ptm-popup");
+
+  const btn = popup
+    .append("button")
+    .attr("class", "icon")
+    .on("click", toggleOptions);
+
+  btn.append("span").attr("class", "text-outline").text("🎤");
+
+  const svg = btn.append("svg").attr("width", "100%").attr("height", "100%");
+
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${outer / 2}, ${outer / 2})`);
+
+  const above = g.append("g").attr("class", "above");
+  above
+    .append("path")
+    .attr("id", "text-arc-above")
+    .attr("d", `M -${w1 / 2} 0 A ${w1 / 2} ${w1 / 2} 0 0 1 ${w1 / 2} 0`);
+
+  const below = g.append("g").attr("class", "below");
+
+  below
+    .append("path")
+    .attr("id", "text-arc-below")
+    .attr("d", `M -${w2 / 2} 0 A ${w2 / 2} ${w2 / 2} 0 0 0 ${w2 / 2} 0`);
+
+  above
+    .append("text")
+    .append("textPath")
+    .attr("xlink:href", "#text-arc-above")
+    .style("text-anchor", "middle")
+    .attr("startOffset", "50%")
+    .text("Pass The Mic");
+
+  below
+    .append("text")
+    .append("textPath")
+    .attr("xlink:href", "#text-arc-below")
+    .style("text-anchor", "middle")
+    .attr("startOffset", "50%")
+    .text("OPTIONS");
+
+  above.attr("transform", "translate(0, 0)");
+  below.attr("transform", "translate(0, 6)");
+
+  const options = popup.append("div").attr("class", "options").html(`
+		<section id="intro">
+			<h2>Pass The Mic</h2>
+			<p class="description">Visualize how much each person is talking in Google Meet</p>
+		</section>
+		
+		<section id="settings">
+			<fieldset>
+				<legend>Settings</legend>
+				<div>
+					<input type="checkbox" id="enable" checked>
+					<label for="enable">Enable</label>
+				</div>
+				<div>
+					<input type="checkbox" id="captions">
+					<label for="captions">Show captions</label>
+				</div>
+			</fieldset>
+		</section>
+
+		<section id="credits">
+			<p>By <a href="https://pudding.cool/author/russell-samora" target="_blank" rel="noreferrer">Russell Samora</a> for <a href="https://pudding.cool" target="_blank" rel="noreferrer">The
+				Pudding</a>.</p>
+		</section>
+	`);
+
+  options
+    .append("button")
+    .text("Close")
+    .on("click", () => options.classed("active", false));
+
+  settings = [...document.querySelectorAll(".ptm-popup .options input")].map(
+    (input) => input.id
+  );
+
+  settings.forEach((key) => {
+    const value = getStorage(key) === "true";
+    document.getElementById(key).checked = value || false;
+    document.getElementById(key).addEventListener("change", (e) => {
+      console.log("change");
+      setStorage(key, e.target.checked);
+      updateOptions();
+    });
+  });
+}
+
+async function init(btn) {
+  createPopup();
+
+  captionsButtonEl = btn;
+  captionsContainerEl = document.querySelector(".a4cQT");
+  // chrome.storage.local.onChanged.addListener(storageChanged);
+
+  updateOptions();
 }
 
 waitForElement("[aria-label='Turn on captions (c)']", init);
